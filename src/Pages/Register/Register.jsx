e762fff8e79fb8510d6c6d8c1b63ea2b13d1ccab
sakibsiddiqi15 <sakibsiddiqi15@gmail.com>
2021-12-15 12:22:14 +0600

    margin change

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,7 +5,7 @@ import RegisterOverview from "./RegisterOverview";
 const Register = () => {
   return (
     <section id="register" className="center vh-min-100 my-5 my-md-3 my-lg-0">
-      <Container className="mx-5 mx-md-0">
+      <Container className="mx-3 mx-md-0">
         <Row xs={1} md={2}>
           <Col className="bg-dark-lt py-4">
             <RegisterOverview />
